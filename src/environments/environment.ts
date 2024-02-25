b315371caddbc26f54407315e775b58b153abe24
Mehdialaoui02 <mehdi2002alaoui@gmail.com>
2024-02-25 18:04:18 +0100

    To do app connected to firebase
    This commit contains everything about the app from the angular components to the environments set up

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDpkzs3k_SMmqxeqRh-9aFeNcjLC6SGVTA",      
+        authDomain: "to-do-app-1a775.firebaseapp.com",      
+        projectId: "to-do-app-1a775",      
+        storageBucket: "to-do-app-1a775.appspot.com",      
+        messagingSenderId: "780378779420",      
+        appId: "1:780378779420:web:cc53fe29b1d2ee1cd494d9"
+      }
+      
+};
